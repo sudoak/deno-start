a6ea8fefcc8abd9f53ad87ad04a081928fbf23b2
sudoak <akram@coffeebeans.io>
2020-05-22 17:27:05 +0530

    [refactor] : comments

--- config/db/index.ts
+++ config/db/index.ts
@@ -1,5 +1,6 @@
 import { MongoClient } from "https://deno.land/x/mongo@v0.7.0/mod.ts";
 
+// intantiate mongo client
 const client = new MongoClient();
 client.connectWithUri("mongodb://localhost:27017");
 
